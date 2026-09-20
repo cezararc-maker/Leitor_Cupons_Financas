eee3f4ff595bbd631852eb407641ea49ca2a5424
cezararc-maker <cezar.arc@gmail.com>
2026-09-19 23:41:43 -0400

    build: adiciona plugin Room para exportar schemas

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("androidx.room") version "2.8.5" apply false
 }
