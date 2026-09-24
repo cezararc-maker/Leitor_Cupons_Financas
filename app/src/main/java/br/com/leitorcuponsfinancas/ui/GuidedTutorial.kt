d17764df0f384b7cbfe7ed38185bf05872f121b0
cezararc-maker <cezar.arc@gmail.com>
2026-09-23 23:20:05 -0400

    fix: corrige import do modifier do tutorial

--- app/src/main/java/br/com/leitorcuponsfinancas/ui/GuidedTutorial.kt
+++ app/src/main/java/br/com/leitorcuponsfinancas/ui/GuidedTutorial.kt
@@ -27,7 +27,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.composed
+import androidx.compose.ui.composed
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
