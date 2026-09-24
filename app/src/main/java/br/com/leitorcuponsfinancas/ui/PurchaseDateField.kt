66ce27d9c2406bb137f7d06fae7fde44db3882a7
cezararc-maker <cezar.arc@gmail.com>
2026-09-23 23:18:41 -0400

    fix: corrige sobreposicao clicavel do campo de data

--- app/src/main/java/br/com/leitorcuponsfinancas/ui/PurchaseDateField.kt
+++ app/src/main/java/br/com/leitorcuponsfinancas/ui/PurchaseDateField.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
@@ -89,7 +88,7 @@ internal fun PurchaseDateField(
 
         Box(
             modifier = Modifier
-                .matchParentSize()
+                .fillMaxSize()
                 .clickable { showPicker = true },
         )
     }
