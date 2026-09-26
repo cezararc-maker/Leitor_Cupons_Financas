9c7c2023831fe6e061c62df8fe9619a41984656b
cezararc-maker <cezar.arc@gmail.com>
2026-09-25 20:02:59 -0400

    security: inicializa identidade local do aparelho

--- app/src/main/java/br/com/leitorcuponsfinancas/LeitorCuponsApplication.kt
+++ app/src/main/java/br/com/leitorcuponsfinancas/LeitorCuponsApplication.kt
@@ -1,5 +1,14 @@
 package br.com.leitorcuponsfinancas
 
 import android.app.Application
+import br.com.leitorcuponsfinancas.data.InstallationIdentityStore
 
-class LeitorCuponsApplication : Application()
+class LeitorCuponsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        InstallationIdentityStore
+            .getInstance(this)
+            .getOrCreateInstallationId()
+    }
+}
